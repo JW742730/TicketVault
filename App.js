d26ae3211e594342c781fb75b25b1fa0d0aaa1fc
Jakobi Washington <JW742730@southeast.edu>
2023-01-31 13:48:20 -0600

    TicketVault

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image, TextInput, Pressable } from 'react-native';
+
+const PlaceholderImage = require('./assets/concert.png');
 
 export default function App() {
+  
+  
+  const onPressCalculate = () => {
+    
+
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.ticketVaultText}>Ticket Vault</Text>
+      <Image source={PlaceholderImage} style={styles.concertContainer} />
+      <Pressable style={styles.container}>
+       <Text style={styles.buttonStyle}>Find The Cost</Text>
+      </Pressable>
+        
       <StatusBar style="auto" />
+      <TextInput
+        style={styles.numberOfTickets}
+        placeholder="Number of Tickets"
+        />
+      
     </View>
   );
 }
@@ -17,4 +36,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  concertContainer: {
+    position: 'absolute',
+    bottom: 10,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    
+  },
+  buttonStyle: {
+    flex: 1,
+    backgroundColor: '#f98b88',
+    fontSize: 30,
+    padding: 7,
+    position: 'absolute',
+    top: 210,
+  },
+  ticketVaultText: {
+    position: 'absolute',
+    fontSize: 50,
+    top: 30
+  },
+  numberOfTickets: {
+    position: 'absolute',
+    top: 120,
+    fontSize: 30,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    padding: 8,
+  }
 });
