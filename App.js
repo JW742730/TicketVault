22ff6fcc0874a8bda1101bb572ff787047a9f414
Jakobi Washington <JW742730@southeast.edu>
2023-02-01 14:19:29 -0600

    First commit

--- App.js
+++ App.js
@@ -1,28 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image, TextInput, Pressable } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+setTimeout(SplashScreen.hideAsync, 2000);
 
 const PlaceholderImage = require('./assets/concert.png');
 
 export default function App() {
   
   
-  const onPressCalculate = () => {
-    
-
-  }
   return (
     <View style={styles.container}>
       <Text style={styles.ticketVaultText}>Ticket Vault</Text>
       <Image source={PlaceholderImage} style={styles.concertContainer} />
-      <Pressable style={styles.container}>
+      <Pressable style={styles.container}
+        onPress={() => {
+          let ticketAmount = 0
+          let ticketCost = ticketAmount * 99.99
+        }}>
        <Text style={styles.buttonStyle}>Find The Cost</Text>
       </Pressable>
-        
+      <Text style={styles.ticketPrice}>Ticket Cost: $299.97</Text>
       <StatusBar style="auto" />
       <TextInput
         style={styles.numberOfTickets}
         placeholder="Number of Tickets"
+        onChangeText={newText => ticketAmount(newText)}
         />
       
     </View>
@@ -55,6 +60,11 @@ const styles = StyleSheet.create({
     position: 'absolute',
     fontSize: 50,
     top: 30
+  },
+  ticketPrice: {
+    position: 'absolute',
+    fontSize: 30,
+    top: 310
   },
   numberOfTickets: {
     position: 'absolute',
